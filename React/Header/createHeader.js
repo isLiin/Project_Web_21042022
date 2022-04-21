c3b3c6fb78a40b8d2ef06e08cb81fad9dd6207e4
KhanhHN9 <hongockhanh.it@gmail.com>
2022-04-21 23:56:55 +0700

    Create Header

---
+++ React/Header/createHeader.js
@@ -0,0 +1,24 @@
+
+export default function PostHeader() {
+    return (
+        <div id="iHeader" class="row">
+            <div class="hide-bgr"></div>
+            <div class="contentHeader">
+                <ul class="itemContainerMenu col-9 d-flex">
+                    <li class="itemMenu"><a href="">Home</a></li>
+                    <li class="itemMenu"><a href="">Destination</a></li>
+                    <li class="itemMenu"><a href="">Near me</a></li>
+                    <li class="itemMenu"><a href="">Events</a></li>
+                    <li class="itemMenu"><a href="">Blog</a></li>
+                    <li class="itemMenu"><a href="">Galley</a></li>
+                    <li class="itemMenu"><a href="">About</a></li>
+                    <li class="itemMenu"><a href="">Contact us</a></li>
+                </ul>
+                <ul class="itemContainerMenu col-3 d-flex">
+                    <li class="itemMenu"><a href="">Login</a></li>
+                    <li class="itemMenu"><a href="">Regist</a></li>
+                </ul>
+            </div>
+        </div>
+    );
+}
